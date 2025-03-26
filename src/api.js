ce7147d6460e372653f83be5ee30e0a73bcfd936
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-03-26 18:06:02 +0100

    Add fetchPokemonList
    We want to be able to search for pokemons so we need to have an api call
    that can fetch the data for it. This does that.

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export async function getPokemon(name) {
+export async function fetchPokemonData(name) {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
@@ -6,5 +6,17 @@ export async function getPokemon(name) {
   const body = await response.json();
   // NOTE: debugging
   console.log(body);
-  return body.name;
+  return body;
+}
+
+export async function fetchPokemonList() {
+  const response = await fetch(
+    "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0",
+  );
+  if (!response.ok) {
+    throw new Error("HTTP error! status: ${response.status}");
+  }
+  const body = await response.json();
+  // data.results is an array of objects { name, url }
+  return body.results;
 }
